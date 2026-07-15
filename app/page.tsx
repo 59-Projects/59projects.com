b85016a9a3f299972de53e0c6541ba6a2afa8b96
Jeremy Zilar <jeremyzilar@gmail.com>
2026-07-15 10:33:41 -0600

    Track page views on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import { getAllProjects, getHome } from "@/lib/content";
 import { HomeView } from "@/components/HomeView";
+import { PageViewTracker } from "@/components/PageViewTracker";
 
 export default async function HomePage() {
   const [projects, home] = await Promise.all([getAllProjects(), getHome()]);
-  return <HomeView home={home} projects={projects} />;
+  return (
+    <>
+      <PageViewTracker event="home_page_viewed" />
+      <HomeView home={home} projects={projects} />
+    </>
+  );
 }
